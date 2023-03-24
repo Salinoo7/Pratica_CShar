0fa96b822c889a69bcd00b856b7ffdb2935ba21a
salinoo7 <lucassalino3008@outlook.com>
2023-03-23 23:39:21 -0300

    Update 23-03
    Update do Exercicio 02

---
+++ Exercicio_02/Media.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exercicio_02
+{
+    public class Media
+    {
+        public double CalcularEstoqueMedio(double valor1, double valor2)
+        {
+            double resultado = (valor1 + valor2)/ 2;
+            return resultado;
+        }
+    }
+}
---
+++ Exercicio_02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System; 
+namespace Exercicio_02
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            int QuantidadeMinima;
+            int QuantidadeMaxima;
+            double EstoqueMedio;
+            Media media = new Media(); 
+
+            Console.WriteLine("====================");
+            Console.WriteLine("ESTOQUE MÉDIO DE PEÇAS");
+            Console.WriteLine("====================");
+            Console.WriteLine();
+            Console.WriteLine("Quantidade minima: ");
+            QuantidadeMinima = int.Parse(Console.ReadLine());
+            Console.WriteLine("Quantidade maxima: ");
+            QuantidadeMaxima = int.Parse(Console.ReadLine());
+            EstoqueMedio = media.CalcularEstoqueMedio(QuantidadeMinima, QuantidadeMaxima);
+            Console.WriteLine($"O estoque médio de peças é de : {EstoqueMedio}");
+
+        }
+    }
+}
