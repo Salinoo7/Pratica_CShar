f0e172db51235c3440c36d70744eac9c8056ee0e
salinoo7 <lucassalino3008@outlook.com>
2023-03-23 23:50:04 -0300

    Update 23-03
    Update Exercicio 03

---
+++ Exercicio_03/Conversor.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exercicio_03
+{
+    public class Conversor
+    {
+        public double ConverterDolar(double valor1, double valor2)
+        {
+            double resultado = (valor1 * valor2);
+            return resultado;
+        }
+    }
+}
